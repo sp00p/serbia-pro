e8ee8d276d6b687d01b66697750c3518ddb8429b
sp00p <46919543+sp00p@users.noreply.github.com>
2019-12-16 18:15:43 -0800

    add chat commands
    chat commands, more to come. possible command handler in future.

--- bot.js
+++ bot.js
@@ -6,4 +6,34 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('guildMemberAdded', () =>{
+  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+
+  if(!channel) return;
+
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+client.on('message', message => {
+
+  const channel = message.channel
+
+  if(message.content.includes('anime')){
+
+    channel.send("fucking you mom");
+
+  }
+
+  if(message.content.startsWith('!stream')){
+
+    channel.send("https://www.twitch.tv/chrometheuss");
+  }
+
+  if(message.content.includes('helloing serbia pro')){
+
+    channel.send('helloing and mansallam bro');
+
+  }
+});
+
 client.login(auth.token);
