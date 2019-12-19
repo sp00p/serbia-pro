5154dd54e0f415d5482d98d75f1b049b31a06f39
sp00p <46919543+sp00p@users.noreply.github.com>
2019-12-18 22:56:54 -0800

    command handler
    create command handler and config file for bot

--- bot.js
+++ bot.js
@@ -1,6 +1,29 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const auth = require('./auth.json');
+const botconfig = require('./botconfig.json');
+const fs = require('fs');
+const prefix = '.';
+client.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) => {
+
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    client.commands.set(props.help.name, props);
+
+  })
+})
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
