73508b6ec16d2684e233f295400a23b05aa993e4
Sean <seancornell11@gmail.com>
2020-01-12 13:31:56 -0800

    add music commands (again)

---
+++ commands/resume.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+
+module.exports.run = async(client, messages, args, ops) => {
+
+  let fetched = ops.active.get(message.guild.id);
+
+  if(!fetched) return message.channel.send("there no music lol!");
+
+  if(message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send("not in same channel as bot");
+
+  if(fetched.dispatcher.paused) return message.channel.send("music is playing already....");
+
+  fetched.dispatcher.resume();
+
+  message.channel.send("playing music again :)");
+}
+
+module.exports.help = {
+  name: "resume"
+}
